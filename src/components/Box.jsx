9fc447cdeaf647b940c0581427483be96c99d6f1
Paul Wang <wang.po.hung@gmail.com>
2022-11-16 19:42:04 -0800

    add texture to cube

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import texture from "../images/rocky_texture.jpg";
+
 function Box() {
+  const colorMap = useLoader(TextureLoader, texture);
   return (
     <mesh rotation={[90, 0, 20]}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="hotpink" />
+      <meshStandardMaterial map={colorMap} />
     </mesh>
   );
 }
